db50b5d2fe7a4ba71e483b6f4f8a1c8231bfc613
sallywang147 <60257613+sallywang147@users.noreply.github.com>
2022-01-22 21:16:30 -0500

    Add files via upload

---
+++ MTP.py
@@ -0,0 +1,21 @@
+import sys
+
+MSGS = ( "some testing string", "avd", "some","check out the string","some strings")
+
+def strxor(a, b):     # xor two strings of different lengths
+    if len(a) > len(b):
+       return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a[:len(b)], b)])
+    else:
+       return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a, b[:len(a)])])
+
+def random(size=16):
+    return open("/dev/urandom").read(size)
+
+def encrypt(key, msg):
+    c = strxor(key, msg)
+    print c.encode('hex')
+    return c
+
+def main():
+    key = random(1024)
+    ciphertexts = [encrypt(key, msg) for msg in MSGS]
